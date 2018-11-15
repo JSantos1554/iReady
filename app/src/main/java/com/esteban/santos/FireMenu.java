4ab9aa3ac7fc031ae2dfa52e25683285cb6ecd50
Jonah <jonahmae.santos.iics@ust.edu.ph>
2018-11-15 22:55:58 +0800

    up

--- app/src/main/java/com/esteban/santos/FireMenu.java
+++ app/src/main/java/com/esteban/santos/FireMenu.java
@@ -1,7 +1,9 @@
 package com.esteban.santos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FireMenu extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class FireMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_menu);
+    }
+
+    public void process(View v) {
+        Intent i = null, chooser = null;
+
+        if (v.getId() == R.id.hotlines) {
+            i = new Intent(this, FireHotlines.class);
+            startActivity(i);
+
+        } else if (v.getId() == R.id.guidelines) {
+            i = new Intent(this, FireGuidelines.class);
+            startActivity(i);
+
+        } else if (v.getId() == R.id.firstaid) {
+            i = new Intent(this, FireFirstAid.class);
+            startActivity(i);
+        }else if (v.getId() == R.id.backbtn) {
+            i = new Intent(this, MainMenu.class);
+            startActivity(i);
+        }
     }
 }
