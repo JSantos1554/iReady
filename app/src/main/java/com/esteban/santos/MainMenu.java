4b6b45bc8daee63d4992d7445d6cebee5a1030fc
Jonah <jonahmae.santos.iics@ust.edu.ph>
2018-11-15 17:21:55 +0800

    Initial commit

---
+++ app/src/main/java/com/esteban/santos/MainMenu.java
@@ -0,0 +1,42 @@
+package com.esteban.santos;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void process(View v) {
+        Intent i = null, chooser = null;
+
+        if (v.getId() == R.id.mapbtn) {
+            i = new Intent(this, MapsActivity.class);
+            startActivity(i);
+
+        } else if (v.getId() == R.id.firebtn) {
+            i = new Intent(this, FireMenu.class);
+            startActivity(i);
+
+
+        } else if (v.getId() == R.id.stormbtn) {
+            i = new Intent(this, StormMenu.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.medicalbtn) {
+            i = new Intent(this, MedicalMenu.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.securitybtn) {
+            i = new Intent(this, SecurityMenu.class);
+            startActivity(i);
+        }
+
+    }
+}
