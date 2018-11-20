460a6075234ad9bf806ea9e7d34265d72a6897f6
Jonah <jonahmae.santos.iics@ust.edu.ph>
2018-11-21 03:52:42 +0800

    update

--- app/src/main/java/com/esteban/santos/FireFirstAid.java
+++ app/src/main/java/com/esteban/santos/FireFirstAid.java
@@ -1,7 +1,10 @@
 package com.esteban.santos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.VideoView;
 
 public class FireFirstAid extends AppCompatActivity {
 
@@ -10,4 +13,18 @@ public class FireFirstAid extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_first_aid);
     }
+
+    public void process(View v) {
+        Intent i = null, chooser = null;
+
+        if (v.getId() == R.id.hotlines) {
+            i = new Intent(this, FireHotlines.class);
+            startActivity(i);
+
+        }else if (v.getId() == R.id.backbtn) {
+            i = new Intent(this, FireMenu.class);
+            startActivity(i);
+        }
+    }
+
 }
--- app/src/main/java/com/esteban/santos/FireHotlines.java
+++ app/src/main/java/com/esteban/santos/FireHotlines.java
@@ -1,7 +1,9 @@
 package com.esteban.santos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FireHotlines extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class FireHotlines extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_hotlines);
+    }
+
+    public void process(View v) {
+        Intent i = null, chooser = null;
+
+        if (v.getId() == R.id.hotlines) {
+            i = new Intent(this, FireHotlines.class);
+            startActivity(i);
+
+        }else if (v.getId() == R.id.backbtn) {
+            i = new Intent(this, FireMenu.class);
+            startActivity(i);
+        }
     }
 }
