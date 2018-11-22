3c362ee98fb91e9095691175bcfc9a7e0a2c02c6
Jonah <jonahmae.santos.iics@ust.edu.ph>
2018-11-22 23:12:47 +0800

    update

--- app/src/main/java/com/esteban/santos/FireFirstAid.java
+++ app/src/main/java/com/esteban/santos/FireFirstAid.java
@@ -1,9 +1,11 @@
 package com.esteban.santos;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class FireFirstAid extends AppCompatActivity {
@@ -12,6 +14,16 @@ public class FireFirstAid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_first_aid);
+
+        VideoView videoView = findViewById(R.id.videoView2);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.firstaidfire;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 
     public void process(View v) {
--- app/src/main/java/com/esteban/santos/FireGuidelines.java
+++ app/src/main/java/com/esteban/santos/FireGuidelines.java
@@ -1,9 +1,13 @@
 package com.esteban.santos;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
+import android.widget.MediaController;
+
 
 public class FireGuidelines extends AppCompatActivity {
 
@@ -11,7 +15,18 @@ public class FireGuidelines extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_guidelines);
+
+        VideoView videoView = findViewById(R.id.videoView2);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.firstaidfire;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
+
     public void process(View v) {
         Intent i = null, chooser = null;
 
