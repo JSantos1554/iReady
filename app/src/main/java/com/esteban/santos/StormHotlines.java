e297d30b07e4dcba3b6bf54ba83f7d7f0d2d4609
Jonah <jonahmae.santos.iics@ust.edu.ph>
2018-12-07 02:31:03 +0800

    update

--- app/src/main/java/com/esteban/santos/StormHotlines.java
+++ app/src/main/java/com/esteban/santos/StormHotlines.java
@@ -1,6 +1,10 @@
 package com.esteban.santos;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,5 +24,55 @@ public class StormHotlines extends AppCompatActivity {
             startActivity(i);
 
         }
+    }
+
+    public void BtnNat_onClick(View view){
+        String number = "911";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        startActivity(intent);
+    }
+
+    public void BtnNationalDis_onClick(View view){
+        String number = "029111406";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        startActivity(intent);
+    }
+
+    public void BtnPCG_onClick(View view){
+        String number = "025278481";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        startActivity(intent);
+    }
+
+    public void BtnPRC_onClick(View view){
+        String number = "143";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        startActivity(intent);
+    }
+
+    public void BtnPAG_onClick(View view){
+        String number = "6324342696";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        startActivity(intent);
     }
 }
